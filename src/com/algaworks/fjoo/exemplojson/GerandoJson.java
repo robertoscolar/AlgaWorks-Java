e1fa6c36770e268509895d8849bdfc3929c34145
Roberto Scolar <robertobs1@hotmail.com>
2021-09-26 19:34:32 -0300

    json

---
+++ src/com/algaworks/fjoo/exemplojson/GerandoJson.java
@@ -0,0 +1,31 @@
+package com.algaworks.fjoo.exemplojson;
+
+import com.algaworks.fjoo.exemplojson.modelo.Cliente;
+import com.algaworks.fjoo.exemplojson.modelo.Endereco;
+import com.algaworks.fjoo.exemplojson.modelo.Telefone;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GerandoJson {
+
+    public static void main(String[] args) {
+
+        Endereco endereco = new Endereco("Rua das águas,10", "Ribeirão Preto", "SP");
+
+        Telefone residencial = new Telefone("RESIDENCIAL", "11 3333-1111");
+        Telefone comercial = new Telefone ("COMERCIAL", "11 4141-4141");
+        List<Telefone> telefones = new ArrayList<>();
+        telefones.add(residencial);
+        telefones.add(comercial);
+
+        Cliente cliente = new Cliente("João da Silva", 28, endereco, telefones);
+
+        Gson gson = new Gson();
+
+        String json = gson.toJson(cliente);
+
+        System.out.println(json);
+    }
+}
