b286c2f7377390f2ab5c2b5db4c2d6889e85d366
Roberto Scolar <robertobs1@hotmail.com>
2021-10-01 03:00:17 -0300

    Paramêtros da JVM

---
+++ src/com/algaworks/fjoo/parametrosjvm/Memoria.java
@@ -0,0 +1,28 @@
+package com.algaworks.fjoo.parametrosjvm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Memoria {
+
+    private List<Object> lista = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        Memoria memoria = new Memoria();
+        int opcao = 0;
+        try (Scanner entrada = new Scanner(System.in)) {
+            do {
+                System.out.println("Informe uma opção: ");
+                opcao = entrada.nextInt();
+
+                if (opcao == 1) {
+                    for (int i = 0; i < 2_000_000; i++) {
+                        memoria.lista.add(new Object());
+                    }
+                }
+            } while (opcao != 0);
+        }
+    }
+}
